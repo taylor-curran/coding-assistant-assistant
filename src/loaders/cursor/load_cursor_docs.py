d0994f63e250d65fb041ca3c47fb99654d3faa3f
Taylor Curran <taylorcurranagtc@gmail.com>
2025-03-12 10:10:31 -0500

    initial cursor docs

---
+++ src/loaders/cursor/load_cursor_docs.py
@@ -0,0 +1,13 @@
+# src/loaders/codeium/load_codeium_docs.py
+
+# src/loaders/codeium/load_codeium_docs.py
+from bs4 import BeautifulSoup
+from prefect import flow
+
+from src.utils.network import fetch, fetch_rendered
+from src.loaders.models.models import DocsPage, CodeAssistantCompany
+
+BASE_URL = "https://docs.cursor.com"
+SITEMAP_URL = f"{BASE_URL}/sitemap.xml"
+
+# TODO: Implement cursor docs loader
